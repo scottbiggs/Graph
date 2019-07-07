c9a4aff5431250fb6d4ed48bd41191cdb363b756
scott-mac <scottmorganbiggs@gmail.com>
2019-07-07 00:37:24 -0500

    testing base cases and new Exceptions for Graph class

--- test.java
+++ test.java
@@ -16,25 +16,62 @@ class test {
 
 	public static void main(String args[]) {
 
-		mGraph = new Graph<>(true);
-		fillGraph();
+		// Testing Minimal Graphs.
+		Graph<MyNode> directed = new Graph<>(true);
+		Graph<MyNode> undirected = new Graph<>(false);
 
-		System.out.println(mGraph.toString());
+		System.out.println("empty directed graph connected? " + directed.isConnected());
+		System.out.println("empty undirected graph connected? " + undirected.isConnected());
+
+		try {
+			directed.addNode(0, new test().new MyNode(50));
+			undirected.addNode(0, new test().new MyNode(500));
+			directed.addNode(2, new test().new MyNode(70));
+			undirected.addNode(2, new test().new MyNode(700));
+			directed.addNode(4, new test().new MyNode(90));
+			undirected.addNode(4, new test().new MyNode(900));
+			undirected.addNode(4, new test().new MyNode(900));
+
+		} catch (GraphNodeDuplicateIdException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("directed graph (1 unconnected node) connected? " + directed.isConnected());
+		System.out.println("undirected graph (1 unconnected node) connected? " + undirected.isConnected());
+
+		directed.addEdge(0, 0);
+		undirected.addEdge(0, 0);
+
+		System.out.println("directed graph (node pointing to itself) connected? " + directed.isConnected());
+		System.out.println("directed graph (node pointing to itself) connected? " + undirected.isConnected());
+
+		directed.addEdge(2, 4);
+		undirected.addEdge(2, 4);
+
+		System.out.println("directed graph (should be no) connected? " + directed.isConnected());
+		System.out.println("directed graph (should be no) connected? " + undirected.isConnected());
 
 
-		System.out.println("connected? " + mGraph.isConnected());
+		// mGraph = new Graph<>(true);
+		// fillGraph();
+		// System.out.println(mGraph.toString());
+		// System.out.println("connected? " + mGraph.isConnected());
 	}
 
 	/** just fills the graph with a bunch of nodes and edges */
 	private static void fillGraph() {
-		mGraph.addNode(0, new test().new MyNode(50));
-		mGraph.addNode(1, new test().new MyNode(100));
-		mGraph.addNode(2, new test().new MyNode(80));
-		mGraph.addNode(3, new test().new MyNode(200));
-		mGraph.addNode(4, new test().new MyNode(75));
-		mGraph.addNode(5, new test().new MyNode(50));
-		mGraph.addNode(6, new test().new MyNode(20));
-		mGraph.addNode(7, new test().new MyNode(5));
+		try {
+			mGraph.addNode(0, new test().new MyNode(50));
+			mGraph.addNode(1, new test().new MyNode(100));
+			mGraph.addNode(2, new test().new MyNode(80));
+			mGraph.addNode(3, new test().new MyNode(200));
+			mGraph.addNode(4, new test().new MyNode(75));
+			mGraph.addNode(5, new test().new MyNode(50));
+			mGraph.addNode(6, new test().new MyNode(20));
+			mGraph.addNode(7, new test().new MyNode(5));
+		} catch (GraphNodeDuplicateIdException e) {
+			e.printStackTrace();
+		}
 
 		mGraph.addEdge(1, 2);
 		mGraph.addEdge(1, 3);
